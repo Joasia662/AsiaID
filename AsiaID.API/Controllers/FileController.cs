b0876386efe4548791d2c64ba90a0531afc0a75e
Joanna Cepuch <asiacepuch1@gmail.com>
2025-04-08 00:21:29 +0200

    add get request for files and error handling

---
+++ AsiaID.API/Controllers/FileController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace AsiaID.API.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+        public FileController(
+            FileExtensionContentTypeProvider fileExtensionContentTypeProvider) 
+        {
+            _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider
+                ?? throw new ArgumentNullException(
+                    nameof(fileExtensionContentTypeProvider));
+        }
+
+        [HttpGet("{fieldId}")]
+        public ActionResult GetFile(string fieldId)
+        {
+            var pathToFile = "test.txt";
+            
+            if (!System.IO.File.Exists(pathToFile))
+            {
+                return NotFound();
+            }
+
+            if (!_fileExtensionContentTypeProvider.TryGetContentType(
+                fieldId, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+            return File(bytes,contentType, Path.GetFileName(pathToFile));
+
+        }
+    }
+}
--- AsiaID.API/Program.cs
+++ AsiaID.API/Program.cs
@@ -1,11 +1,35 @@
+using Microsoft.AspNetCore.StaticFiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    //when a customer ask for certain representation, if e not support it there will be a message;
+    options.ReturnHttpNotAcceptable = true;
+}).AddXmlDataContractSerializerFormatters();
+
+//-- Adding problem details into failed http request
+//builder.Services.AddProblemDetails(options =>
+//{
+//    options.CustomizeProblemDetails = ctx =>
+//    {
+//        ctx.ProblemDetails.Extensions.Add("additionalInfo",
+//            "Additional info example");
+//        ctx.ProblemDetails.Extensions.Add("server", Environment.MachineName);
+//    };
+//});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//--It is for reading type of files if we return one
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
+
 
 var app = builder.Build();
 
